1efe4dd9ba979cd48cdd6be63766cf040e50cd51
yoyo837 <yoyo837@hotmail.com>
2020-03-16 18:06:54 +0800

    add offsetY

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -80,6 +80,7 @@ export interface TableProps<RecordType>
   scroll?: Omit<RcTableProps<RecordType>['scroll'], 'y'> & {
     scrollToFirstRowOnChange?: boolean;
     y?: number | true;
+    offsetY?: number;
   };
   sortDirections?: SortOrder[];
   showSorterTooltip?: boolean;
@@ -476,7 +477,8 @@ function Table<RecordType extends object = any>(props: TableProps<RecordType>) {
                 (window.parseFloat(cpStyle.getPropertyValue('margin-top')) || 0) +
                 (window.parseFloat(cpStyle.getPropertyValue('margin-bottom')) || 0);
               return prev + height;
-            }, 0),
+            }, 0) +
+            (props.scroll.offsetY || 0),
         );
       }
     }, 100),
